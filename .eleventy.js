85e7cb9b7c125e19dedbec69c947e43e73d0f507
atglenn <adam.glenn@gmail.com>
2023-08-03 23:45:40 -0400

    Adding stuff.

--- .eleventy.js
+++ .eleventy.js
@@ -102,18 +102,22 @@ module.exports = function(eleventyConfig) {
     // .sort((a, b) => a.data.lastName - b.data.lastName)
     .sort((a, b) => {
       if (a.data.value === b.data.value) {
-        if (a.data.year === b.data.year) {
-          if (a.data.brand === b.data.brand) {
-            if (a.data.subset === b.data.subset) {
-              return a.data.lastName < b.data.lastName ? -1 : 1
+        if (a.data.sport === b.data.sport) {
+          if (a.data.year === b.data.year) {
+            if (a.data.brand === b.data.brand) {
+              if (a.data.subset === b.data.subset) {
+                return a.data.lastName < b.data.lastName ? -1 : 1
+              } else {
+                return a.data.subset < b.data.subset ? -1 : 1
+              }
             } else {
-              return a.data.subset < b.data.subset ? -1 : 1
+              return a.data.brand < b.data.brand ? -1 : 1
             }
           } else {
-            return a.data.brand < b.data.brand ? -1 : 1
+            return a.data.year < b.data.year ? -1 : 1
           }
         } else {
-          return a.data.year < b.data.year ? -1 : 1
+          return a.data.sport < b.data.sport ? -1 : 1
         }
       } else {
         return a.data.value > b.data.value ? -1 : 1
