89ed83313ab80c9f39edfc47e75fbd4c25826573
atglenn <adam.glenn@gmail.com>
2023-08-03 23:03:16 -0400

    Adding stuff.

--- .eleventy.js
+++ .eleventy.js
@@ -96,6 +96,31 @@ module.exports = function(eleventyConfig) {
     })
   })
 
+  eleventyConfig.addCollection('mostValuableCards', function(collectionApi) {
+    return collectionApi
+    .getFilteredByGlob('_cards/**/*.md')
+    // .sort((a, b) => a.data.lastName - b.data.lastName)
+    .sort((a, b) => {
+      if (a.data.value === b.data.value) {
+        if (a.data.year === b.data.year) {
+          if (a.data.brand === b.data.brand) {
+            if (a.data.subset === b.data.subset) {
+              return a.data.lastName < b.data.lastName ? -1 : 1
+            } else {
+              return a.data.subset < b.data.subset ? -1 : 1
+            }
+          } else {
+            return a.data.brand < b.data.brand ? -1 : 1
+          }
+        } else {
+          return a.data.year < b.data.year ? -1 : 1
+        }
+      } else {
+        return a.data.value > b.data.value ? -1 : 1
+      }
+    })
+  })
+
   return {
     htmlTemplateEngine: "liquid"
   }
